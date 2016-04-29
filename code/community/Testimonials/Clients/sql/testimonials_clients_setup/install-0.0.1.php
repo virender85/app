dfde07b46f50bbee41afe1b0ec07a624bb762e8b
virender85 <viru.sing6@gmail.com>
2016-04-29 22:39:06 +0530

    Update install-0.0.1.php

--- code/community/Testimonials/Clients/sql/testimonials_clients_setup/install-0.0.1.php
+++ code/community/Testimonials/Clients/sql/testimonials_clients_setup/install-0.0.1.php
@@ -1,29 +1,12 @@
 <?php
 $this->startSetup();
 
-/**
- * Note: there are many ways in Magento to achieve the same result below
- * of creating a database table. For this tutorial we have gone with the 
- * Varien_Db_Ddl_Table method but feel free to explore what Magento do in
- * CE 1.8.0.0 and ealier versions if you are interested.
- */
+
 $table = new Varien_Db_Ddl_Table();
 
-/**
- * This is an alias to the real name of our database table, which is 
- * configured in config.xml. By using an alias we can reference the same
- * table throughout our code if we wish and if the table name ever had to 
- * change we could simply update a single location, config.xml
- * - testimonials_clients is the model alias
- * - client is the table reference
- */
 $table->setName($this->getTable('testimonials_clients/client'));
 
-/**
- * Add the columns we need for now. If you need more in the future you can
- * always create a new setup script as an upgrade, we will introduce that 
- * later on in the tutorial.
- */
+
 $table->addColumn(
     'entity_id',
     Varien_Db_Ddl_Table::TYPE_INTEGER, 
